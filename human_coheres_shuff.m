875b86b2929012d81cdfd1a52efd363075338e39
hsw28 <holdbackaglacier@yahoo.com>
2021-02-18 16:14:44 -0600

    added coherence shuffle

---
+++ human_coheres_shuff.m
@@ -0,0 +1,72 @@
+function fs = human_coheres_shuff(lfpone, lfptwo, lowband, highband, window_in_sec, same)
+	%MAKE SURE YOU PUT IN DOWNSAMPLED DATA
+	%shuffles both LFPs
+%put in two signals. filter them before if ya want
+% input low and high bands you wanna look at
+% input time in seconds, then does it in windows overlapping by half the period
+%if you want to compare the same set of channels against each other just put the set for lfpone and lfptwo
+%for input same, put 0 if lfps are not same and 1 if same
+
+channellength = length(lfpone);
+i = window_in_sec*2000;
+halfi = i./2;
+
+for c=1:size(lfpone,1);
+	n = length(lfpone) ;
+	idx = randperm(n);
+	b = lfpone(c,:);
+	b(1,idx) = lfpone(c,:);
+	lfpone(c,:) = b;
+end
+
+%for c=1:size(lfptwo,1);
+%	n = length(lfptwo) ;
+%	idx = randperm(n);
+%	b = lfptwo(c,:);
+%	b(1,idx) = lfptwo(c,:);
+%	lfptwo(c,:) = b;
+%end
+
+curtime = 0;
+allpairs = NaN(size(lfpone,1),size(lfpone,1));
+%first channel is c and second channel is n;
+for c=1:size(lfpone,1);
+		if same==1
+			startc = c+1;
+		else
+			startc = c;
+		end
+			for n=startc:size(lfpone,1);
+		allcoh = []; %reset for new channel pair
+		curtime = 1;
+		while curtime<=channellength-i;
+
+			[wcoh,wcs,f] = wcoherence(lfpone(c,curtime:curtime+i), lfptwo(n,curtime:curtime+i), i);
+			%find INDEX of values of frequency in theta band
+			indx = find(f>lowband & f<highband);
+			%those index values are the ones we wanna keep for wcoh
+			wcoh = wcoh(indx, :);
+			allcoh = horzcat(allcoh,wcoh);
+			curtime = curtime+halfi;
+
+		end
+
+		%find average coherence for the pair
+		meancoh = mean(mean(allcoh, 1));
+		%fill into matrix
+		allpairs(c,n) = meancoh;
+	end %finished computing for pair of interest
+
+end %finished computing for all pairs
+
+fs = allpairs;
+
+% average across theta freq
+% might not want to do this. not clear yet
+
+
+
+
+
+%freq = f(indx);
+%coh = allcoh;
